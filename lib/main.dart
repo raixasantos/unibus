09c35dea6ff4ca0f1e93a86f1b8656947d383f1e
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-09-25 08:58:09 -0300

    criado prototipo da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unibus/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(),
     );
   }
 }
