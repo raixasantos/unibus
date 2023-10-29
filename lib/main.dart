87640fe218b0e7cd1d94ad66f30417ace5311b34
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-10-29 15:22:30 -0300

    criado provider de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:unibus/firebase_options.dart';
+import 'package:unibus/providers/cadastro_provider.dart';
 import 'package:unibus/screens/login.dart';
 import 'package:unibus/theme.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CadastroProvider(),
+      child: MyApp()
+    )
+  );
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
---
+++ lib/providers/cadastro_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+class CadastroProvider with ChangeNotifier {
+  String nome = '';
+  String matricula = '';
+  String faculdade = '';
+  String turno = '';
+  String numeroCarteira = '';
+  String password = '';
+}
