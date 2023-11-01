58b843eb33c0129c0b6d97df1b80053567610f66
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2023-11-01 02:36:11 -0300

    feat: remove, update rotas

--- lib/services/routes_services.dart
+++ lib/services/routes_services.dart
@@ -1,42 +1,74 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:unibus/constants/errors.dart';
 import 'package:unibus/models/route_bus.dart';
-import 'package:unibus/utils/uuid.dart';
 
 class RouteServices {
-  final String tablename = "routes";
+  final CollectionReference routesCollection =
+      FirebaseFirestore.instance.collection("routes");
 
   Future getRoutes() async {
-    return await FirebaseFirestore.instance.collection(tablename).get();
+    return await routesCollection.get();
   }
 
-  Future getRoute(int id) async {
-    return await FirebaseFirestore.instance
-        .collection(tablename)
-        .where("id", isEqualTo: id)
-        .get();
+  Future getRoute(String name) async {
+    return await routesCollection.where("name", isEqualTo: name).get();
   }
 
   Future<void> addRoute(RouteBus route) async {
-    String uuid = generateUUID();
     try {
-      final CollectionReference routesCollection =
-          FirebaseFirestore.instance.collection(tablename);
-
       final existingRoute =
           await routesCollection.where('name', isEqualTo: route.name).get();
 
       if (existingRoute.docs.isEmpty) {
-        await routesCollection.add({
-          'code_bus': route.codeBus,
-          'description': route.description,
-          'id': uuid,
-          'name': route.name
-        });
+        await routesCollection.add(
+          {
+            'code_bus': route.codeBus,
+            'description': route.description,
+            'name': route.name
+          },
+        );
       } else {
-        throw Exception('A rota já existe.');
+        throw Exception(ErrorExists("A rota"));
       }
     } catch (e) {
-      throw Exception('Erro ao cadastrar rota: $e');
+      throw Exception(ErrorAdd("rota", e));
+    }
+  }
+
+  Future<void> removeRoute(RouteBus route) async {
+    try {
+      final routeDocument = await routesCollection.doc(route.name).get();
+
+      if (routeDocument.exists) {
+        await routesCollection.doc(route.name).delete();
+      } else {
+        throw Exception(ErrorNotExists('A rota'));
+      }
+    } catch (e) {
+      throw Exception(ErrorRemove("rota", e));
+    }
+  }
+
+  Future<void> updateRoute(RouteBus route) async {
+    try {
+      final existingRoute =
+          await routesCollection.where('name', isEqualTo: route.name).get();
+
+      if (existingRoute.docs.isNotEmpty) {
+        final docId = existingRoute.docs[0].id;
+
+        await routesCollection.doc(docId).update(
+          {
+            'code_bus': route.codeBus,
+            'description': route.description,
+            'name': route.name,
+          },
+        );
+      } else {
+        throw Exception(ErrorNotFound("A rota"));
+      }
+    } catch (e) {
+      throw Exception(ErrorUpdate("rota", e));
     }
   }
 }
