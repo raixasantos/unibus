2506fafe3abd2b651084376d96674d007bd8b3f4
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-12-01 22:16:15 -0300

    adicionada imagem de usuário ao motorista

--- lib/models/Motorista.dart
+++ lib/models/Motorista.dart
@@ -3,8 +3,8 @@ import 'package:unibus/models/Usuario.dart';
 class Motorista extends Usuario {
   int _numeroCarteira;
 
-  Motorista(String nome, String password, this._numeroCarteira)
-      : super(nome, password);
+  Motorista(String nome, String password, this._numeroCarteira, {imageUrl})
+      : super(nome, password, imageUrl: imageUrl);
 
   set numeroCarteira(int numeroCarteira) {
     _numeroCarteira = numeroCarteira;
